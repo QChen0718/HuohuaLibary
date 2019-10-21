e6bb44c66315051754877ded1cdd9ceb17d5063c
陈庆 <chenqing873@1caifu.com>
2019-10-21 16:43:04 +0800

    初次提交

---
+++ CommonModule/CommonModule.h
@@ -0,0 +1,19 @@
+//
+//  CommonModule.h
+//  CommonModule
+//
+//  Created by 陈庆 on 2019/10/21.
+//  Copyright © 2019 陈庆. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CommonModule.
+FOUNDATION_EXPORT double CommonModuleVersionNumber;
+
+//! Project version string for CommonModule.
+FOUNDATION_EXPORT const unsigned char CommonModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonModule/PublicHeader.h>
+
+
