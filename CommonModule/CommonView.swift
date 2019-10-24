68dd524e6ffca6009729d5f2c58d024c550e3ef0
陈庆 <chenqing873@1caifu.com>
2019-10-24 11:17:22 +0800

    创建通用文件

---
+++ CommonModule/CommonView.swift
@@ -0,0 +1,21 @@
+//
+//  CommonView.swift
+//  CommonModule
+//
+//  Created by 陈庆 on 2019/10/24.
+//  Copyright © 2019 陈庆. All rights reserved.
+//
+
+import UIKit
+
+open class CommonView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
